66cb9a0947c0e86344f8751f3a4814da68c1e53f
steffen911 <steffenschmitz@hotmail.de>
2018-10-17 16:58:18 +0200

    read all datasets into the java runtime and parse whole xml structure

--- src/main/java/de/uni_mannheim/informatik/dws/wdi/WDI_Project_T2/IR_App.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/WDI_Project_T2/IR_App.java
@@ -11,14 +11,33 @@ import java.io.File;
 
 public class IR_App {
 
-    private static final Logger logger = WinterLogManager.activateLogger("default");
+    public static final Logger logger = WinterLogManager.activateLogger("default");
 
     public static void main(String[] args) throws Exception {
         // Load the datasets
         logger.info("Loading datasets...");
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         HashedDataSet<Car, Attribute> carEmissions = new HashedDataSet<>();
-        new CarXMLReader().loadFromXML(new File(classloader.getResource("car_emissions.xml").getFile()), "/target/car", carEmissions);
-        logger.info("Successfully loaded car_emissions.xml");
+        logger.info("Loading car_emissions_target...");
+        new CarXMLReader().loadFromXML(new File(classloader.getResource("car_emissions_target.xml").getFile()), "/target/car", carEmissions);
+
+        HashedDataSet<Car, Attribute> offers = new HashedDataSet<>();
+        logger.info("Loading offer_target 1 and 2...");
+        new CarXMLReader().loadFromXML(new File(classloader.getResource("offer_target_1.xml").getFile()), "/target/car", offers);
+        new CarXMLReader().loadFromXML(new File(classloader.getResource("offer_target_2.xml").getFile()), "/target/car", offers);
+
+        HashedDataSet<Car, Attribute> regionEmissions = new HashedDataSet<>();
+        logger.info("Loading region_emissions_target...");
+        new CarXMLReader().loadFromXML(new File(classloader.getResource("region_emissions_target.xml").getFile()), "/target/car", regionEmissions);
+
+        HashedDataSet<Car, Attribute> stations = new HashedDataSet<>();
+        logger.info("Loading station_target...");
+        new CarXMLReader().loadFromXML(new File(classloader.getResource("station_target.xml").getFile()), "/target/car", stations);
+
+        HashedDataSet<Car, Attribute> vehicles = new HashedDataSet<>();
+        logger.info("Loading vehicles_target...");
+        new CarXMLReader().loadFromXML(new File(classloader.getResource("vehicles_target.xml").getFile()), "/target/car", vehicles);
+
+        logger.info("Successfully loaded data sets");
     }
 }
--- src/main/java/de/uni_mannheim/informatik/dws/wdi/WDI_Project_T2/model/CarXMLReader.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/WDI_Project_T2/model/CarXMLReader.java
@@ -1,29 +1,108 @@
 package de.uni_mannheim.informatik.dws.wdi.WDI_Project_T2.model;
 
-import de.uni_mannheim.informatik.dws.winter.model.DataSet;
-import org.w3c.dom.Node;
-
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
 import de.uni_mannheim.informatik.dws.winter.model.io.XMLMatchableReader;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /** A XMLMatchableReader for Cars. */
 public class CarXMLReader extends XMLMatchableReader<Car, Attribute> {
 
-    @Override
-    protected void initialiseDataset(DataSet<Car, Attribute> dataset) {
-        super.initialiseDataset(dataset);
-    }
-
+    /** Create a new car from the given xml node. */
     @Override
     public Car createModelFromElement(Node node, String provenance) {
-        String id = getValueFromChildElement(node, "id");
+        Element elem = (Element) node;
+        String id = elem.getAttribute("id");
+
         Car car = new Car(id, provenance);
         car.setModel(getValueFromChildElement(node, "model"));
         car.setManufacturer(getValueFromChildElement(node, "manufacturer"));
+        car.setFuelType(getValueFromChildElement(node, "fuelType"));
+        car.setTransmission(getValueFromChildElement(node, "transmission"));
 
-        // TODO: Read additional properties into object
+        int horsePower;
+        try {
+            horsePower = Integer.valueOf(getValueFromChildElement(node, "horse-power"));
+        } catch (Exception e) {
+            horsePower = 0;
+        }
+        car.setHorsePower(horsePower);
+
+        double emissions;
+        try {
+            emissions = Double.valueOf(getValueFromChildElement(node, "emissions"));
+        } catch (Exception e) {
+            emissions = 0.0;
+        }
+        car.setEmission(emissions);
+
+        double mileage;
+        try {
+            mileage = Double.valueOf(getValueFromChildElement(node, "mileage"));
+        } catch (Exception e) {
+            mileage = 0.0;
+        }
+        car.setMileage(mileage);
+
+        NodeList children = node.getChildNodes();
+        for (int i = 0; i < children.getLength(); i++) {
+            Node child = children.item(i);
+            if (child.getNodeName().equals("pollution")) {
+                car.setPollution(getPollution(child, id, provenance));
+            }
+            if (child.getNodeName().equals("region")) {
+                car.setRegion(getRegion(child, id, provenance));
+            }
+        }
 
         return car;
+    }
+
+    /** getPollution takes a pollution xml node and extracts the specific values. */
+    private Pollution getPollution(Node node, String id, String provenance) {
+        Pollution pollution = new Pollution(id, provenance);
+        pollution.setPollutant(getValueFromChildElement(node, "pollutant"));
+        pollution.setAqUnit(getValueFromChildElement(node, "aq-unit"));
+
+        double aq;
+        try {
+            aq = Double.valueOf(getValueFromChildElement(node, "air-quality"));
+        } catch (Exception e) {
+            aq = 0.0;
+        }
+        pollution.setAirQuality(aq);
+        return pollution;
+    }
+
+    /** getRegion takes a region xml node and extracts the specific values. */
+    private Region getRegion(Node node, String id, String provenance) {
+        Region region = new Region(id, provenance);
+        region.setStationId(getValueFromChildElement(node, "stationId"));
+        region.setCity(getValueFromChildElement(node, "city"));
+
+        // Latitude and Longitude make only sense together. So either both compile or none
+        double latitude;
+        double longitude;
+        try {
+            latitude = Double.valueOf(getValueFromChildElement(node, "latitude"));
+            longitude = Double.valueOf(getValueFromChildElement(node, "longitude"));
+        } catch (Exception e) {
+            latitude = 0;
+            longitude = 0;
+        }
+        region.setLatitude(latitude);
+        region.setLongitude(longitude);
+
+        int zipCode;
+        try {
+            zipCode = Integer.valueOf(getValueFromChildElement(node, "zip-code"));
+        } catch (Exception e) {
+            zipCode = 0;
+        }
+        region.setZipCode(zipCode);
+
+        return region;
     }
 
 }
