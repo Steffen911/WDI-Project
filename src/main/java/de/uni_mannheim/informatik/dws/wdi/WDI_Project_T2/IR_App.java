ba87755721cc3bab89fa86a1dfcbb586da6cfb45
steffen911 <steffenschmitz@hotmail.de>
2018-10-22 22:43:42 +0200

    add gold standard and move data sets into another dir

--- src/main/java/de/uni_mannheim/informatik/dws/wdi/WDI_Project_T2/IR_App.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/WDI_Project_T2/IR_App.java
@@ -19,24 +19,24 @@ public class IR_App {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         HashedDataSet<Car, Attribute> carEmissions = new HashedDataSet<>();
         logger.info("Loading car_emissions_target...");
-        new CarXMLReader().loadFromXML(new File(classloader.getResource("car_emissions_target.xml").getFile()), "/target/car", carEmissions);
+        new CarXMLReader().loadFromXML(new File(classloader.getResource("data/car_emissions_target.xml").getFile()), "/target/car", carEmissions);
 
         HashedDataSet<Car, Attribute> offers = new HashedDataSet<>();
         logger.info("Loading offer_target 1 and 2...");
-        new CarXMLReader().loadFromXML(new File(classloader.getResource("offer_target_1.xml").getFile()), "/target/car", offers);
-        new CarXMLReader().loadFromXML(new File(classloader.getResource("offer_target_2.xml").getFile()), "/target/car", offers);
+        new CarXMLReader().loadFromXML(new File(classloader.getResource("data/offer_target_1.xml").getFile()), "/target/car", offers);
+        new CarXMLReader().loadFromXML(new File(classloader.getResource("data/offer_target_2.xml").getFile()), "/target/car", offers);
 
         HashedDataSet<Car, Attribute> regionEmissions = new HashedDataSet<>();
         logger.info("Loading region_emissions_target...");
-        new CarXMLReader().loadFromXML(new File(classloader.getResource("region_emissions_target.xml").getFile()), "/target/car", regionEmissions);
+        new CarXMLReader().loadFromXML(new File(classloader.getResource("data/region_emissions_target.xml").getFile()), "/target/car", regionEmissions);
 
         HashedDataSet<Car, Attribute> stations = new HashedDataSet<>();
         logger.info("Loading station_target...");
-        new CarXMLReader().loadFromXML(new File(classloader.getResource("station_target.xml").getFile()), "/target/car", stations);
+        new CarXMLReader().loadFromXML(new File(classloader.getResource("data/station_target.xml").getFile()), "/target/car", stations);
 
         HashedDataSet<Car, Attribute> vehicles = new HashedDataSet<>();
         logger.info("Loading vehicles_target...");
-        new CarXMLReader().loadFromXML(new File(classloader.getResource("vehicles_target.xml").getFile()), "/target/car", vehicles);
+        new CarXMLReader().loadFromXML(new File(classloader.getResource("data/vehicles_target.xml").getFile()), "/target/car", vehicles);
 
         logger.info("Successfully loaded data sets");
     }
