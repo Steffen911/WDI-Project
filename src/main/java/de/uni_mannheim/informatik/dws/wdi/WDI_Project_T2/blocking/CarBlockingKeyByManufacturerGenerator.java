9224b663b82b7b0ef298ef7de5e951aeb1daf436
Steffen Schmitz <steffenschmitz@hotmail.de>
2018-10-25 22:16:38 +0200

    try blocking with levenshtein in mini-batches (#2)

---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/WDI_Project_T2/blocking/CarBlockingKeyByManufacturerGenerator.java
@@ -0,0 +1,62 @@
+package de.uni_mannheim.informatik.dws.wdi.WDI_Project_T2.blocking;
+
+import de.uni_mannheim.informatik.dws.wdi.WDI_Project_T2.model.Car;
+import de.uni_mannheim.informatik.dws.winter.matching.blockers.generators.RecordBlockingKeyGenerator;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.Pair;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.processing.DataIterator;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+import de.uni_mannheim.informatik.dws.winter.similarity.string.TokenizingJaccardSimilarity;
+
+public class CarBlockingKeyByManufacturerGenerator extends RecordBlockingKeyGenerator<Car, Attribute> {
+
+    private TokenizingJaccardSimilarity sim = new TokenizingJaccardSimilarity();
+    private String[] manufacturers = {
+            "audi",
+            "bmw",
+            "fiat",
+            "ford",
+            "honda",
+            "hyundai",
+            "jeep",
+            "mazda",
+            "mercedes_benz",
+            "mini",
+            "nissan",
+            "opel",
+            "peugeot",
+            "renault",
+            "seat",
+            "skoda",
+            "smart",
+            "volkswagen"
+    };
+
+    @Override
+    public void generateBlockingKeys(
+            Car car,
+            Processable<Correspondence<Attribute, Matchable>> correspondences,
+            DataIterator<Pair<String, Car>> resultCollector
+    ) {
+        int key = getIdForManufacturer(car.getManufacturer());
+        if (key < 0) {
+            // ignore manufacturers that are not whitelisted
+            return;
+        }
+        resultCollector.next(new Pair<>(Integer.toString(key), car));
+    }
+
+    private int getIdForManufacturer(String manufacturer) {
+        manufacturer = manufacturer.toLowerCase();
+        for (int i = 0; i < manufacturers.length; i++) {
+            double similarity = sim.calculate(manufacturers[i], manufacturer);
+            if (similarity > 0.3) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/WDI_Project_T2/comparator/CarTransmissionComparatorLevenshtein.java
@@ -0,0 +1,58 @@
+package de.uni_mannheim.informatik.dws.wdi.WDI_Project_T2.comparator;
+
+import de.uni_mannheim.informatik.dws.wdi.WDI_Project_T2.model.Car;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.Comparator;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.ComparatorLogger;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.similarity.string.LevenshteinSimilarity;
+
+public class CarTransmissionComparatorLevenshtein implements Comparator<Car, Attribute> {
+
+    private LevenshteinSimilarity sim = new LevenshteinSimilarity();
+    private ComparatorLogger compLogger;
+
+    @Override
+    public double compare(
+            Car c1,
+            Car c2,
+            Correspondence<Attribute, Matchable> schemaCorrespondence
+    ) {
+
+        String m1 = c1.getTransmission();
+        String m2 = c2.getTransmission();
+
+        if (this.compLogger != null) {
+            this.compLogger.setComparatorName(getClass().getName());
+            this.compLogger.setRecord1Value(m1);
+            this.compLogger.setRecord2Value(m2);
+        }
+
+        // preprocessing
+        m1 = (m1 != null) ? m1.toLowerCase() : "";
+        m2 = (m2 != null) ? m2.toLowerCase() : "";
+
+        // calculate similarity
+        double similarity = sim.calculate(m1, m2);
+
+        if(this.compLogger != null){
+            this.compLogger.setRecord1PreprocessedValue(m1);
+            this.compLogger.setRecord2PreprocessedValue(m2);
+            this.compLogger.setSimilarity(Double.toString(similarity));
+        }
+
+        return similarity;
+
+    }
+
+    @Override
+    public ComparatorLogger getComparisonLog() {
+        return this.compLogger;
+    }
+
+    @Override
+    public void setComparisonLog(ComparatorLogger comparatorLog) {
+        this.compLogger = comparatorLog;
+    }
+}
