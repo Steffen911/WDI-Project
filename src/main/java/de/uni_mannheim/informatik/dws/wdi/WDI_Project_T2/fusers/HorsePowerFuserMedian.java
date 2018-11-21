070af1ac3aa11a52f8693b6464ed40a08b9cc5fb
Phares13 <37227568+Phares13@users.noreply.github.com>
2018-11-21 10:27:24 +0100

    add fueltype, horsepower, manufacturer and model fusers

---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/WDI_Project_T2/fusers/HorsePowerFuserMedian.java
@@ -0,0 +1,49 @@
+package de.uni_mannheim.informatik.dws.wdi.WDI_Project_T2.fusers;
+
+import de.uni_mannheim.informatik.dws.wdi.WDI_Project_T2.model.Car;
+import de.uni_mannheim.informatik.dws.winter.datafusion.AttributeValueFuser;
+import de.uni_mannheim.informatik.dws.winter.datafusion.conflictresolution.ConflictResolutionFunction;
+import de.uni_mannheim.informatik.dws.winter.datafusion.conflictresolution.numeric.Average;
+import de.uni_mannheim.informatik.dws.winter.datafusion.conflictresolution.numeric.Median;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.FusedValue;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.RecordGroup;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+
+public class HorsePowerFuserMedian extends AttributeValueFuser<Double, Car, Attribute>{
+
+	public HorsePowerFuserMedian(ConflictResolutionFunction<Double, Car, Attribute> conflictResolution) {
+		super(new Average() );
+	}
+	
+
+	
+	
+	@Override
+	public void fuse(RecordGroup<Car, Attribute> group, Car fusedRecord,Processable<Correspondence<Attribute, Matchable>> correspondence, Attribute elem) {
+		 FusedValue<Double, Car, Attribute> fused = getFusedValue(group, correspondence, elem);
+	        fusedRecord.setHorsePower( fused.getValue().intValue());		
+	}
+	
+	@Override
+	public Double getValue(Car c, Correspondence<Attribute, Matchable> correspondence) {
+		return (double) c.getHorsePower();
+	}
+	
+	
+	@Override
+	public boolean hasValue(Car c, Correspondence<Attribute, Matchable> correspondence) {
+		
+		return c.hasValue(c.HORSE_POWER);
+	}
+	
+
+	
+
+
+
+
+
+}
---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/WDI_Project_T2/fusers/ModelFuserLongestString.java
@@ -0,0 +1,40 @@
+package de.uni_mannheim.informatik.dws.wdi.WDI_Project_T2.fusers;
+
+import de.uni_mannheim.informatik.dws.wdi.WDI_Project_T2.model.Car;
+import de.uni_mannheim.informatik.dws.winter.datafusion.AttributeValueFuser;
+import de.uni_mannheim.informatik.dws.winter.datafusion.conflictresolution.ConflictResolutionFunction;
+import de.uni_mannheim.informatik.dws.winter.datafusion.conflictresolution.string.LongestString;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.FusedValue;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.RecordGroup;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+
+public class ModelFuserLongestString extends AttributeValueFuser<String, Car, Attribute> {
+
+	public ModelFuserLongestString(ConflictResolutionFunction<String, Car, Attribute> conflictResolution) {
+		super(new LongestString());
+		
+	}
+	
+	@Override
+	public String getValue(Car c, Correspondence<Attribute, Matchable> correspondence) {
+		return c.getModel();
+	}
+	
+	@Override
+	public void fuse(RecordGroup<Car, Attribute> group, Car fusedRecord,Processable<Correspondence<Attribute, Matchable>> correspondence, Attribute elem) {
+		 FusedValue<String, Car, Attribute> fused = getFusedValue(group, correspondence, elem);
+	        fusedRecord.setModel(fused.getValue());		
+	}
+	
+	@Override
+	public boolean hasValue(Car c, Correspondence<Attribute, Matchable> correspondence) {
+		
+		return c.hasValue(c.MODEL);
+	}
+	
+	
+
+}
