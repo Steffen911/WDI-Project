e5fbb8b150de6e151cf48d82c6ed6eab10a9ada1
Steffen Schmitz <steffenschmitz@hotmail.de>
2018-11-21 11:16:37 +0100

    write fusion output to xml (#34)

---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/WDI_Project_T2/model/CarXMLFormatter.java
@@ -0,0 +1,39 @@
+package de.uni_mannheim.informatik.dws.wdi.WDI_Project_T2.model;
+
+import de.uni_mannheim.informatik.dws.winter.model.io.XMLFormatter;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * <car id="offer_66">
+ * 		<manufacturer>bmw</manufacturer>
+ * 		<model>BMW_325_i_Cabrio_wenig_Kilometer 3er</model>
+ * 		<fuelType>benzin</fuelType>
+ * 		<transmission>manuell</transmission>
+ * 		<horse-power>218</horse-power>
+ * 		<region>
+ * 			<zip-code>1129</zip-code>
+ * 		</region>
+ * 		<mileage>50000</mileage>
+ * 	</car>
+ */
+public class CarXMLFormatter extends XMLFormatter<Car> {
+
+    @Override
+    public Element createRootElement(Document doc) {
+        return doc.createElement("target");
+    }
+
+    @Override
+    public Element createElementFromRecord(Car c, Document doc) {
+        Element car = doc.createElement("car");
+        car.appendChild(createTextElement("manufacturer", c.getManufacturer(), doc));
+        car.appendChild(createTextElement("model", c.getModel(), doc));
+        car.appendChild(createTextElement("fuelType", c.getFuelType(), doc));
+        car.appendChild(createTextElement("transmission", c.getTransmission(), doc));
+        car.appendChild(createTextElement("horse-power", "" + c.getHorsePower(), doc));
+        car.appendChild(createTextElement("mileage", "" + c.getMileage(), doc));
+        return car;
+    }
+
+}
