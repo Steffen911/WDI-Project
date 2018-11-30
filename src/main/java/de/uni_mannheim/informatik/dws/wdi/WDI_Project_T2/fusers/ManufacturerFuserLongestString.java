b7280950226dc08ff77f5491f130b4efbdeff2b1
Steffen Schmitz <steffenschmitz@hotmail.de>
2018-11-30 22:25:20 +0100

    fix lowercase function in fusers (#42)

--- src/main/java/de/uni_mannheim/informatik/dws/wdi/WDI_Project_T2/fusers/ManufacturerFuserLongestString.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/WDI_Project_T2/fusers/ManufacturerFuserLongestString.java
@@ -30,7 +30,7 @@ public class ManufacturerFuserLongestString extends AttributeValueFuser<String, 
     public void fuse(RecordGroup<Car, Attribute> group, Car fusedRecord, Processable<Correspondence<Attribute, Matchable>> correspondence, Attribute elem) {
         FusedValue<String, Car, Attribute> fused = getFusedValue(group, correspondence, elem);
         fusedRecord.setAttributeProvenance(Car.MANUFACTURER, fused.getOriginalIds());
-        fusedRecord.setManufacturer(fused.getValue().toLowerCase());
+        fusedRecord.setManufacturer((fused.getValue() != null) ? fused.getValue().toLowerCase() : fused.getValue());
     }
 
 }
--- src/main/java/de/uni_mannheim/informatik/dws/wdi/WDI_Project_T2/fusers/ModelFuserSplitIntersection.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/WDI_Project_T2/fusers/ModelFuserSplitIntersection.java
@@ -2,30 +2,35 @@ package de.uni_mannheim.informatik.dws.wdi.WDI_Project_T2.fusers;
 
 import de.uni_mannheim.informatik.dws.wdi.WDI_Project_T2.model.Car;
 import de.uni_mannheim.informatik.dws.winter.datafusion.AttributeValueFuser;
-import de.uni_mannheim.informatik.dws.winter.datafusion.conflictresolution.string.ShortestString;
+import de.uni_mannheim.informatik.dws.winter.datafusion.conflictresolution.list.Intersection;
 import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
 import de.uni_mannheim.informatik.dws.winter.model.FusedValue;
 import de.uni_mannheim.informatik.dws.winter.model.Matchable;
 import de.uni_mannheim.informatik.dws.winter.model.RecordGroup;
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
 import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+import org.apache.commons.lang3.StringUtils;
 
-public class ModelFuserShortestString extends AttributeValueFuser<String, Car, Attribute> {
+import java.util.Arrays;
+import java.util.List;
 
-    public ModelFuserShortestString() {
-        super(new ShortestString());
+public class ModelFuserSplitIntersection extends AttributeValueFuser<List<String>, Car, Attribute> {
+
+    public ModelFuserSplitIntersection() {
+        super(new Intersection());
+        this.setCollectDebugResults(false);
     }
 
     @Override
-    public String getValue(Car c, Correspondence<Attribute, Matchable> correspondence) {
-        return c.getModel();
+    public List<String> getValue(Car c, Correspondence<Attribute, Matchable> correspondence) {
+        return Arrays.asList(c.getModel().split("[_\\s]"));
     }
 
     @Override
     public void fuse(RecordGroup<Car, Attribute> group, Car fusedRecord, Processable<Correspondence<Attribute, Matchable>> correspondence, Attribute elem) {
-        FusedValue<String, Car, Attribute> fused = getFusedValue(group, correspondence, elem);
+        FusedValue<List<String>, Car, Attribute> fused = getFusedValue(group, correspondence, elem);
         fusedRecord.setAttributeProvenance(Car.MODEL, fused.getOriginalIds());
-        fusedRecord.setModel((fused.getValue() != null) ? fused.getValue().toLowerCase() : fused.getValue());
+        fusedRecord.setModel(StringUtils.join(fused.getValue(), " "));
     }
 
     @Override
